e589241658cba9de5954d3e694479433963269fe
arimaze <adrian.foata@gmail.com>
2017-11-14 20:25:00 +0100

    maj de la commande

--- panel.php
+++ panel.php
@@ -1,7 +1,8 @@
 Welcome on Cripsy server panel :
 
    <form method="post">
-       <input type="submit" name="test" id="test" value="Start" style="margin-top:30px" /><br/>
+       <input type="submit" name="test" id="test" value="Start server" style="margin-top:30px" /><br/>
+      <input type="submit" name="stop" id="Stop" value="Stop Server" style="margin-top:30px"/><br/>
       <input type="submit" name="update" id="Stop and update" value="Update Server" style="margin-top:30px"/><br/>
    </form>
 
@@ -14,6 +15,11 @@ Welcome on Cripsy server panel :
    if(array_key_exists('update',$_POST)){
 
       update();
+   }
+
+   if(array_key_exists('stop',$_POST)){
+
+      stop();
    }
 
    function execInBackground($cmd) {
@@ -32,7 +38,8 @@ Welcome on Cripsy server panel :
 
    function testfun()
    {
-       $cmd= 'c:\conan2\ConanSandboxServer.exe -MaxPlayers=50 -nosteamclient -game -server';
+       //$cmd= 'c:\conan2\ConanSandboxServer.exe -MaxPlayers=50 -nosteamclient -game -server';
+       $cmd ='schtasks /Run /TN "Conan Exiles Ping"';
        execInBackground($cmd);
        echo "Conan is restarting please wait few minutes";
    }
@@ -46,6 +53,12 @@ Welcome on Cripsy server panel :
       echo "Conan will be updated ! Please wait 5 minutes, and then START server <br/>";
       $cmd= 'c:/steamcmd/steamcmd.exe +login anonymous +force_install_dir "C:\conan2" +app_update 443030 validate +quit exit';
       execInBackground($cmd);
+   }
+
+   function stop()
+   {
+     exec('taskkill /f /IM ConanSandboxServer-Win64-Test.exe', $output);
+     print_r("Conan killed ! ");
    }
 
    ?>
